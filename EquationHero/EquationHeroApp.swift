039fddc5839bfe61ce79f9730046bf9367356fa0
LC Chang <lcchang0000@gmail.com>
2024-08-05 15:44:56 +0800

    Initial Commit

---
+++ EquationHero/EquationHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquationHeroApp.swift
+//  EquationHero
+//
+//  Created by LuChin Chang on 2024/8/5.
+//
+
+import SwiftUI
+
+@main
+struct EquationHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
